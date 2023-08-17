f2efad3e38d7727cf27b8cbff9e007cee4924b93
golovewkin <golovewkin@gmail.com>
2023-08-16 21:43:34 -0600

    add range example

--- 4-lists.py
+++ 4-lists.py
@@ -11,3 +11,19 @@ print(example_list[-1])
 
 # using function
 thislist = list(("apple", "banana", "cherry"))  # note the double round-brackets
+for example in thislist:
+    print(example)
+
+# check if elem in list
+print('apple' in thislist)
+print('apple2' in thislist)
+
+# shortcuts
+# The return value is a new list, leaving the old list unchanged
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]  # newlist = [expression for item in iterable if condition == True]
+print(newlist)
+
+# how to copy array
+copy_of_fruits = [x for x in fruits]
+print(copy_of_fruits)
