b2ef6ac04dba98a707f024f5b28a3d2bd1e8ce7a
golovewkin <golovewkin@gmail.com>
2023-08-18 18:49:52 -0600

    true random examples add

--- 19-random.py
+++ 19-random.py
@@ -1,7 +1,7 @@
 # Can we make truly random numbers?
 # Yes. In order to generate a truly random number on our computers we need to get the random data from some outside source. This outside source is generally our keystrokes, mouse movements, data on network etc
-import random
+import random as r
 
-x = random.randint(100)
+x = r.randint(100)
 
 print(x)
